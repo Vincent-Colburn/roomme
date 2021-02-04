72fc7f50ffbb8c4976ae01ed3d5a8aa5a99136b8
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-02-04 16:59:50 -0700

    LOGO!!! some profile updates

--- roomme.client/src/pages/ProfilePage.vue
+++ roomme.client/src/pages/ProfilePage.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is the about page</h1>
+  <div class="profile container-fluid">
+    <div class="row text-center my-3">
+      <div class="col">
+        <h1>WELCOME "NAME"!</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="offset-2 col-2">
+        <img src="https://thispersondoesnotexist.com/image" class="img-fluid" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
