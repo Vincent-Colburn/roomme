c6103bcb58d26ff4912e13de02e563ba3159e05a
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-02-05 15:24:41 -0700

    functioning profile page

--- roomme.client/src/AppState.js
+++ roomme.client/src/AppState.js
@@ -3,5 +3,10 @@ import { reactive } from 'vue'
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
   user: {},
-  account: {}
+  account: {},
+  matches: [],
+  activeMatch: {},
+  profiles: [],
+  myProfile: { name: 'vincent', age: 23, gender: 'Male', Location: '83703', aboutMe: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit porro impedit placeat libero a sapiente distinctio dignissimos totam eaque rerum? Explicabo, labore quasi quam fuga quas voluptatum quia repellendus tempore eius similique perferendis dolor dicta dolorem quo corrupti. Reiciendis ipsa voluptatum modi quia dolores corrupti natus illum earum nulla! Cupiditate aspernatur tenetur qui quaerat eveniet laudantium maiores error odio. Eius hic tempora cupiditate laboriosam asperiores numquam blanditiis, unde, aut aliquam molestiae vitae consequatur minus nam! Nobis ab minus recusandae ex dolor aut et id alias doloremque reprehenderit, maxime sequi corrupti fugiat aliquam nemo sed qui nulla nam unde illum! Cumque.', imgUrl: 'https://thispersondoesnotexist.com/image', lookingFor: 'roommate', priceRange: '300-600', interests: 'Lorem ipsum dolor sit amet, consectetur.', lifestyleOptions: 'smoker, toker, midnight joker', anticipatedMoveInDate: '12/1/2040' },
+  activeProfile: {}
 })
--- roomme.client/src/pages/ProfilePage.vue
+++ roomme.client/src/pages/ProfilePage.vue
@@ -2,19 +2,116 @@
   <div class="profile container-fluid">
     <div class="row text-center my-3">
       <div class="col">
-        <h1>WELCOME "NAME"!</h1>
+        <h1>WELCOME ThE ThUnDer dOMe!</h1>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editModal">
+          Edit Profile
+        </button>
       </div>
     </div>
     <div class="row">
       <div class="offset-2 col-2">
-        <img src="https://thispersondoesnotexist.com/image" class="img-fluid" alt="">
+        <img :src="state.profile.imgUrl" class="img-fluid" alt="">
+      </div>
+      <div class="col water offset-1">
+        <div class="row my-2">
+          <div class="col">
+            <h3>Name: {{ state.profile.name }}</h3>
+          </div>
+        </div>
+        <div class="row my-2">
+          <div class="col">
+            <h3>Age: {{ state.profile.age }}</h3>
+          </div>
+        </div>
+        <div class="row my-2">
+          <div class="col">
+            <h3>Gender: {{ state.profile.gender }}</h3>
+          </div>
+        </div>
+        <div class="row my-2">
+          <div class="col">
+            <h3>Interests: {{ state.profile.interests }}</h3>
+          </div>
+        </div>
+        <div class="row my-2">
+          <div class="col">
+            <h3>Price range: {{ state.profile.priceRange }}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row my-5">
+      <div class="col"></div>
+      <div class="col-9">
+        <h4>About Me:</h4>
+        <p>{{ state.profile.aboutMe }}</p>
+      </div>
+      <div class="col"></div>
+    </div>
+    <div class="row">
+      <div class="col"></div>
+      <div class="col-9">
+        <h4>
+          Lifestyle Choices:
+        </h4>
+        <p>
+          {{ state.profile.lifestyleOptions }}
+        </p>
+      </div>
+      <div class="col"></div>
+    </div>
+  </div>
+  <!-- Modal -->
+  <div class="modal fade"
+       id="editModal"
+       tabindex="-1"
+       role="dialog"
+       aria-labelledby="exampleModalLabel"
+       aria-hidden="true"
+  >
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">
+            Modal title
+          </h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <img src="https://4rfnv3jdfte8qj2229aqgj4h-wpengine.netdna-ssl.com/wp-content/uploads/2019/02/15610552_web1_Cull2-Sealion-20feb19-1024x683.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">
+            Save changes
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { computed, reactive } from 'vue'
+import { AppState } from '../AppState'
 export default {
-  name: 'Profile'
+  name: 'Profile',
+  setup() {
+    const state = reactive({
+      profile: computed(() => AppState.myProfile)
+    })
+    return {
+      state
+    }
+  }
 }
 </script>
+<style lang="scss" scoped>
+  // .water{
+  //   background-image: url("../assets/img/rm-logo.png");
+  // }
+</style>
